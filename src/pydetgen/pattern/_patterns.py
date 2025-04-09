1bc4d1b7e78e9894babe475896699adaa24239c4
Fang Han <fhanonline@gmail.com>
2025-04-09 17:10:26 -0400

    update _patterns.py

--- src/pydetgen/pattern/_patterns.py
+++ src/pydetgen/pattern/_patterns.py
@@ -2,20 +2,22 @@ import numpy as np
 
 
 def checkerboard(
-
-
-    startxy: tuple[float, float], pitchxy: tuple[float, float], nxy: tuple[int, int]
+    startxy: np.ndarray,
+    pitchxy: np.ndarray,
+    nxy: np.ndarray,
 ) -> np.ndarray:
     """
     Generate checkerboard pattern centroid coordinates.
 
     Parameters
     ----------
-    startxy : tuple of float
+    startxy : numpy.ndarray, shape=(2,), dtype=float64
         Starting point of the checkerboard pattern (x, y).
-    pitchxy : tuple of float
+
+    pitchxy : numpy.ndarray, shape=(2,), dtype=float64
         Pitch of the checkerboard pattern (x, y).
-    nxy : tuple of int
+
+    nxy : numpy.ndarray, shape=(2,), dtype=int32
         Number of checkerboard squares in the x and y directions.
 
     Returns
@@ -27,9 +29,11 @@ def checkerboard(
     """
 
     firstrow = np.array(
-        np.meshgrid(startxy[0], np.arange(nxy[1]//2) * pitchxy[1] * 2 + startxy[1], 0)
+        np.meshgrid(startxy[0], np.arange(nxy[1] // 2) * pitchxy[1] * 2 + startxy[1], 0)
     ).reshape(3, -1)
     pos = []
     for irow in range(nxy[0]):
-        pos.append(firstrow.T + np.array([irow * pitchxy[0], (irow % 2) * pitchxy[0], 0]))
+        pos.append(
+            firstrow.T + np.array([irow * pitchxy[0], (irow % 2) * pitchxy[0], 0])
+        )
     return np.array(pos).T.reshape(3, -1)
